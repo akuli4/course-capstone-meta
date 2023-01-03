d9b68ce45ec73aef9c09cafe5c95094b4ef1c286
Alan Ksenzov <fkdrsp@proton.me>
2023-01-03 14:19:58 +0100

    created nav links

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,47 @@ import Nav from "./components/generic/Nav";
 import Main from "./components/generic/Main";
 import Footer from "./components/generic/Footer";
 
+const APP_LINKS = [
+	{
+		title: "Home",
+		url: "#",
+	},
+	{
+		title: "About",
+		url: "#",
+	},
+	{
+		title: "Menu",
+		url: "#",
+	},
+	{
+		title: "Reservations",
+		url: "#",
+	},
+	{
+		title: "Order online",
+		url: "#",
+	},
+	{
+		title: "Login",
+		url: "#",
+	},
+];
+
 function App() {
 	return (
 		<>
 			<Header>
-				<Nav></Nav>
+				<img src="/" alt="Little Lemon" />
+				<Nav>
+					<ul>
+						{APP_LINKS.map((link, index) => (
+							<li key={index}>
+								<a href={link.url}>{link.title}</a>
+							</li>
+						))}
+					</ul>
+				</Nav>
 			</Header>
 			<Main></Main>
 			<Footer></Footer>
