190adee2bd17a32d08a1fa2be084f9637ff9151a
Alan Ksenzov <fkdrsp@proton.me>
2023-01-07 20:52:32 +0100

    Wrapped App with BookingState

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
+import BookingState from "./state/BookingState";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Router>
-			<App />
+			<BookingState>
+				<App />
+			</BookingState>
 		</Router>
 	</React.StrictMode>
 );
