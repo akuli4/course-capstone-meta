40411795be89a51873220a83085dd0dfed938616
Alan Ksenzov <fkdrsp@proton.me>
2023-01-07 20:45:15 +0100

    Inserted booking form

--- src/components/booking/BookingForm.jsx
+++ src/components/booking/BookingForm.jsx
@@ -8,6 +8,47 @@ const BookingForm = ({
 }) => {
 	return (
 		<div>
+			<section id="bookingForm">
+				<form action="" onSubmit={handleSubmit}>
+					<label htmlFor="res-date">Choose date</label>
+					<input
+						type="date"
+						id="res-date"
+						value={formData["res-date"]}
+						onChange={handleFormChange}
+					/>
+					<label htmlFor="res-time">Choose time</label>
+					<select
+						id="res-time"
+						value={formData["res-time"]}
+						onChange={handleFormChange}
+					>
+						{availableTimes.map((visitTime, index) => (
+							<option key={index}>{visitTime}</option>
+						))}
+					</select>
+					<label htmlFor="guests">Number of guests</label>
+					<input
+						type="number"
+						placeholder="1"
+						min="1"
+						max="10"
+						id="guests"
+						value={formData["guests"]}
+						onChange={handleFormChange}
+					/>
+					<label htmlFor="occasion">Occasion</label>
+					<select
+						id="occasion"
+						value={formData["occasion"]}
+						onChange={handleFormChange}
+					>
+						<option>Birthday</option>
+						<option>Anniversary</option>
+					</select>
+					<input type="submit" value="Make Your reservation" />
+				</form>
+			</section>
 		</div>
 	);
 };
