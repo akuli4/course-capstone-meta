d3e4a2dfdaa5331835317ffc86610bf423f533ed
Alan Ksenzov <fkdrsp@proton.me>
2023-01-08 14:16:56 +0100

    Created test for choosing time

--- src/__tests__/booking_tests.test.js
+++ src/__tests__/booking_tests.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import BookingState from "../state/BookingState";
 import Booking from "../pages/Booking";
 
@@ -14,4 +14,14 @@ test("Renders the BookingForm heading", () => {
 	init();
 	const headingElement = screen.getByText(/Book now/);
 	expect(headingElement).toBeInTheDocument();
+});
+
+test("Booking time is updated", () => {
+	init();
+	const timeSelector = screen.getByLabelText(/Choose time/);
+	const timeOptions = screen.getAllByTestId(/time-option/);
+
+	fireEvent.change(timeSelector, { target: { value: timeOptions[3].value } });
+
+	expect(timeOptions[3].selected).toBeTruthy();
 });
