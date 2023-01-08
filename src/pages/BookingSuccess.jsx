52db18c4e74a7689af7f7951261051b7f4fcfd37
Alan Ksenzov <fkdrsp@proton.me>
2023-01-08 20:10:35 +0100

    Date is formatted in local locale

--- src/pages/BookingSuccess.jsx
+++ src/pages/BookingSuccess.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import intlFormat from "date-fns/intlFormat";
 import { useSearchParams } from "react-router-dom";
 const BookingSuccess = () => {
 	const [searchParams] = useSearchParams();
+	const date = new Date(searchParams.get("date"));
 	return (
 		<div>
 			<h3>Thank you!</h3>
 			<p>Your booking is done!</p>
+			<p>Your booking date is</p>
 			<p>
-				Your booking date is{" "}
-				{searchParams.get("date") ?? "could not be retrieved"}
+				{intlFormat(date, {
+					weekday: "long",
+					year: "numeric",
+					day: "numeric",
+					month: "long",
+				}) ?? "could not be retrieved"}
 			</p>
 		</div>
 	);
