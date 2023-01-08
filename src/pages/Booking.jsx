42af48181a684220ca980849f2ecf4fc84e83344
Alan Ksenzov <fkdrsp@proton.me>
2023-01-08 14:07:20 +0100

    Added "Book now" heading as it was required by tests

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -4,6 +4,9 @@ import BookingForm from "../components/booking/BookingForm";
 const Booking = () => {
 	return (
 		<section id="booking" aria-label="Booking page">
+			<div className="heading">
+				<h3>Book now</h3>
+			</div>
 			<BookingForm />
 		</section>
 	);
