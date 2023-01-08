7a34951bb0fe7a0961d5d1fcbceef42e6f8b29ee
Alan Ksenzov <fkdrsp@proton.me>
2023-01-08 19:21:44 +0100

    Created a case for updating available times

--- src/state/availableTimesReducer.jsx
+++ src/state/availableTimesReducer.jsx
@@ -1,5 +1,10 @@
 export default (state, action) => {
 	switch (action.type) {
+		case "SET_AVAILABLE_TIMES":
+			return {
+				...state,
+				available: action.payload,
+			};
 		default:
 			return { ...state };
 	}
