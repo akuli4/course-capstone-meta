35ec847688487347f7a176c8c180df9b5701476a
Alan Ksenzov <fkdrsp@proton.me>
2023-01-08 14:09:22 +0100

    Created init function to omit code repitition

--- src/__tests__/booking_tests.test.js
+++ src/__tests__/booking_tests.test.js
@@ -1,12 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import BookingState from "../state/BookingState";
 import Booking from "../pages/Booking";
-test("Renders the BookingForm heading", () => {
-	render(
+
+function init() {
+	return render(
 		<BookingState>
 			<Booking />
 		</BookingState>
 	);
+}
+
+test("Renders the BookingForm heading", () => {
+	init();
 	const headingElement = screen.getByText(/Book now/);
 	expect(headingElement).toBeInTheDocument();
 });
