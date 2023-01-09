2311bc70c0284b18d8bfa3ba9d6c32514af09bd4
Alan Ksenzov <fkdrsp@proton.me>
2023-01-09 16:01:06 +0100

    Created validation schema using Yup

--- src/components/booking/BookingForm.jsx
+++ src/components/booking/BookingForm.jsx
@@ -3,9 +3,10 @@ import Input from "../util/form/Input";
 import Select from "../util/form/Select";
 import { useBooking } from "../../state/BookingState";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const BookingForm = ({}) => {
-	const { formData, availableTimes, onChange, onSubmit } = useBooking();
+	const { availableTimes, onSubmit: submitForm } = useBooking();
 	const formik = useFormik({
 		initialValues: {
 			"res-date": "",
@@ -14,13 +15,30 @@ const BookingForm = ({}) => {
 			occasion: "Birthday",
 		},
 		onSubmit(values) {
-			alert(JSON.stringify(values));
+			submitForm(values);
 		},
+		validationSchema: Yup.object({
+			"res-date": Yup.date()
+				.min(new Date(), "Please choose appropriate date")
+				.max(
+					new Date().getFullYear() + 1,
+					"We only have bookings for the current year"
+				)
+				.required("Choose a date for your visit"),
+			"res-time": Yup.string()
+				.matches(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/)
+				.required("Choose time for your visit"),
+			guests: Yup.number()
+				.min(1, "Minimum number of guests is 1")
+				.max(10, "Maximum number of guests is 10")
+				.required("Provide guest count"),
+			occasion: Yup.string().required("Provide occasion"),
+		}),
 	});
 	return (
 		<>
 			<section id="bookingForm">
-				<form action="" onSubmit={onSubmit}>
+				<form action="" onSubmit={formik.handleSubmit}>
 					<Input
 						type={"date"}
 						id={"res-date"}
@@ -56,8 +74,8 @@ const BookingForm = ({}) => {
 
 					<button
 						type="submit"
-						// disabled={!formData.isValid}
-						// aria-disabled={!formData.isValid}
+						disabled={!(formik.isValid && formik.dirty)}
+						aria-disabled={!(formik.isValid && formik.dirty)}
 						aria-label="Reserve a table"
 					>
 						Make Your reservation
