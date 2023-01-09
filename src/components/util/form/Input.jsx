57de7d6b649842f782d044118e504694b95aaf45
Alan Ksenzov <fkdrsp@proton.me>
2023-01-09 16:04:27 +0100

    Created custom case for date input to dynamically update available times

--- src/components/util/form/Input.jsx
+++ src/components/util/form/Input.jsx
@@ -1,6 +1,31 @@
 import React from "react";
 import { useBooking } from "../../../state/BookingState";
 
+const Input = ({ type, id, title, formData, min, max }) => {
+	const { onChange } = useBooking();
+	if (type === "date") {
+		function handleChange(e) {
+			onChange(e);
+			formData.handleChange(e);
+		}
+		return (
+			<div className="form-control">
+				<label htmlFor={id}>{title}</label>
+				<input
+					type={type}
+					id={id}
+					min={min}
+					max={max}
+					{...formData.getFieldProps(id)}
+					onChange={handleChange}
+				/>
+				{formData.errors[id] && formData.touched[id] && (
+					<p>{formData.errors[id]}</p>
+				)}
+			</div>
+		);
+	}
+	// Number
 	if (min && max) {
 		return (
 			<div className="form-control">
