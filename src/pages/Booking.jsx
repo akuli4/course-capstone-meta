ea0ce74b9bf30be3fd698d40b4ce77837554f7e4
Alan Ksenzov <fkdrsp@proton.me>
2023-01-09 17:49:54 +0100

    Moved book now heading one level below

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import BookingForm from "../components/booking/BookingForm";
-
 const Booking = () => {
 	return (
-		<section id="booking" aria-label="Booking page">
-			<div className="heading">
-				<h3>Book now</h3>
-			</div>
 			<BookingForm />
 		</section>
 	);
