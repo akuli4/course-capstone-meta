64fde197b7c9ddf2f38c36ac90e76f5c23341565
Alan Ksenzov <fkdrsp@proton.me>
2023-01-09 17:50:17 +0100

    Added styling to booking section

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import BookingForm from "../components/booking/BookingForm";
+import styles from "../../lib/utils.module.css";
 const Booking = () => {
 	return (
+		<section
+			id="booking"
+			aria-label="Booking page"
+			className={`${styles.vh} ${styles.minW40} ${styles.minW40}`}
+		>
 			<BookingForm />
 		</section>
 	);
