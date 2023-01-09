84fccba01eccaf3433a23cc235c2e699989db393
Alan Ksenzov <fkdrsp@proton.me>
2023-01-09 17:49:24 +0100

    Added visual feedback for form errors

--- src/components/util/form/Select.jsx
+++ src/components/util/form/Select.jsx
@@ -1,24 +1,41 @@
 import React from "react";
+import styles from "./input.module.css";
 
 const Select = ({ id, title, formData, availableTimes, onChange }) => {
 	if (title === "Occasion") {
 		return (
-			<div className="form-control">
+			<div className={styles.control}>
 				<label htmlFor={id}>{title}</label>
-				<select id={id} {...formData.getFieldProps(id)}>
+				<select
+					id={id}
+					{...formData.getFieldProps(id)}
+					className={
+						formData.errors[id] &&
+						formData.touched[id] &&
+						styles.inputError
+					}
+				>
 					<option value="Birthday">Birthday</option>
 					<option value="Anniversary">Anniversary</option>
 				</select>
 				{formData.errors[id] && formData.touched[id] && (
-					<p>{formData.errors[id]}</p>
+					<p className={styles.error}>{formData.errors[id]}</p>
 				)}
 			</div>
 		);
 	}
 	return (
-		<div className="form-control">
+		<div className={styles.control}>
 			<label htmlFor={id}>{title}</label>
-			<select id={id} {...formData.getFieldProps(id)}>
+			<select
+				id={id}
+				{...formData.getFieldProps(id)}
+				className={
+					formData.errors[id] &&
+					formData.touched[id] &&
+					styles.inputError
+				}
+			>
 				{availableTimes.map((visitTime, index) => (
 					<option key={index} data-testid="time-option">
 						{visitTime}
@@ -26,7 +43,7 @@ const Select = ({ id, title, formData, availableTimes, onChange }) => {
 				))}
 			</select>
 			{formData.errors[id] && formData.touched[id] && (
-				<p>{formData.errors[id]}</p>
+				<p className={styles.error}>{formData.errors[id]}</p>
 			)}
 		</div>
 	);
