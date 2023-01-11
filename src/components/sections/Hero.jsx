5163bed6c097b2638c56043da288c72f12683509
Alan Ksenzov <fkdrsp@proton.me>
2023-01-11 17:47:06 +0100

    Reworked: hero section design

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Button from "../util/button/Button";
 import sectionStyles from "./sections.module.css";
-
+import styles from "../../../lib/hero.module.css";
 const Hero = () => {
 	return (
 		<section className={sectionStyles.home}>
-			<div className="wrapper">
-				<div className="home-left">
-					<div className="text-box">
+			<div className={styles.wrapper}>
+				<div className={styles.homeLeft}>
+					<div className={styles.textBox}>
 						<h1>Little Lemon</h1>
 						<h3>Chicago</h3>
 						<p>
@@ -17,17 +17,17 @@ const Hero = () => {
 						</p>
 					</div>
 					<div className="hero-btn-box">
-						<Button text={"Reserve a Table"} />
+						<Button text="Reserve a Table" variant="primary" />
 					</div>
 				</div>
-				<div className="home-right">
+				<div className={styles.homeRight}>
 					<img
 						src="/restauranfood.jpg"
 						alt="Special dish"
 						loading="lazy"
 					/>
 				</div>
-				<div className="background"></div>
+				<span className="background"></span>
 			</div>
 		</section>
 	);
