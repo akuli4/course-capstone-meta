ab8ec0281c0794d6e635eba1d6517a31b1f39bd7
Alan Ksenzov <fkdrsp@proton.me>
2023-01-12 11:32:48 +0100

    Rework: done with testimonials design

--- src/components/sections/Testimonials.jsx
+++ src/components/sections/Testimonials.jsx
@@ -1,36 +1,41 @@
 import React from "react";
 import Testimonial from "../util/testimonial/Testimonial";
 import sectionStyles from "./sections.module.css";
+import styles from "../../../lib/testimonials.module.css";
 
 const FAKE_RATINGS = [
 	{
 		id: "1",
-		name: "Name",
-		rating: "3",
-		comment: "Review text",
+		name: "Judah",
+		rating: "4",
+		comment:
+			"This is a pearl! The food is marvelous & the service is personal, engaging & excellent, having a good knowledge of what . . .",
+		image: "/customer1.png",
 	},
 	{
 		id: "2",
-		name: "Name",
+		name: "Darianna",
 		rating: "5",
-		comment: "Review text",
+		comment:
+			"We had the salmon and the deer and we loved both. Very good quality and kind service. We were happy with the total experience.",
+		image: "/customer2.png",
 	},
 	{
 		id: "3",
-		name: "Name",
+		name: "Kalisha",
 		rating: "4.5",
-		comment: "Review text",
+		comment:
+			"Had lunch at this cosy place on our last day of staying in Chicago. Tasty lunch and good service. We had a vegetarian and a . . .",
+		image: "/customer3.png",
 	},
 ];
 
 const Testimonials = () => {
 	return (
 		<section className={sectionStyles.testimonials}>
-			<div className="wrapper">
-				<div className="top">
+			<div className={styles.wrapper}>
+				<div className={styles.inner}>
 					<h1>Testimonials</h1>
-				</div>
-				<div className="bottom">
 					<ul>
 						{FAKE_RATINGS.map((testimonial) => (
 							<Testimonial
--- src/components/util/testimonial/Testimonial.jsx
+++ src/components/util/testimonial/Testimonial.jsx
@@ -1,22 +1,23 @@
 import React from "react";
+import styles from "./testimonial.module.css";
+
 const Testimonial = ({ data }) => {
 	return (
-		<li>
-			<div className="top">
+		<li className={styles.wrapper}>
+			<div className={styles.top}>
 				<h3>{data.rating}</h3>
+				<img src="/icons/star.png" alt="" />
 			</div>
-			<div className="mid">
+			<div className={styles.mid}>
 				<div className="image-box">
 					<img src={data.image} alt={data.name} loading="lazy" />
 				</div>
-				<div className="name">
+				<div className={styles.name}>
 					<p>{data.name}</p>
 				</div>
 			</div>
-			<div className="bot">
-				<div className="comment">
-					<p>{data.comment}</p>
-				</div>
+			<div className={styles.bot}>
+				<p>{data.comment}</p>
 			</div>
 		</li>
 	);
