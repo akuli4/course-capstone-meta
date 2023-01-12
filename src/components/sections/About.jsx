34084ca846aefd29061bc0bba81f7c7944ef927f
Alan Ksenzov <fkdrsp@proton.me>
2023-01-12 12:25:46 +0100

    Rework: done with about design

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,38 +1,27 @@
 import React from "react";
 import sectionStyles from "./sections.module.css";
+import styles from "../../../lib/about.module.css";
 
 const About = () => {
 	return (
 		<section className={sectionStyles.about} id="about">
-			<div className="wrapper">
-				<div className="left">
-					<div className="head">
-						<h1>Little Lemon</h1>
-						<p>Chicago</p>
-					</div>
-					<div className="body">
-						<p>
-							Little Lemon is a family owned Mediterranean
-							restaurant, focused on traditional recipies served
-							with a modern twist.
-						</p>
-					</div>
+			<div className={styles.wrapper}>
+				<div className={styles.textBox}>
+					<h1>Little Lemon</h1>
+					<h3>Chicago</h3>
+					<p>
+						Little Lemon is a family owned Mediterranean restaurant,
+						focused on traditional recipies served with a modern
+						twist.
+					</p>
 				</div>
-				<div className="right">
-					<div className="magic-box">
-						<div className="top">
-							<img
-								loading="lazy"
-								src="/Mario and Adrian A.jpg"
-								alt="Mario and Adrian"
-							/>
+				<div className={styles.right}>
+					<div className={styles.magicBox}>
+						<div className={styles.top}>
+							<div alt="Mario and Adrian"></div>
 						</div>
-						<div className="bottom">
-							<img
-								loading="lazy"
-								src="restaurant chef B.jpg"
-								alt="Little Lemon's Chef"
-							/>
+						<div className={styles.bottom}>
+							<div></div>
 						</div>
 					</div>
 				</div>
