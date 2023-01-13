9058c9f954368c155b67fd23be22857fe1053399
Alan Ksenzov <fkdrsp@proton.me>
2023-01-13 15:30:36 +0100

    Rework: done with booking confirmation

--- src/pages/BookingSuccess.jsx
+++ src/pages/BookingSuccess.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import intlFormat from "date-fns/intlFormat";
 import { useSearchParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-
+import sectionStyles from "../../lib/utils.module.css";
+import styles from "../../lib/booking.module.css";
 const BookingSuccess = () => {
 	const [searchParams] = useSearchParams();
 	const date = new Date(searchParams.get("date"));
 	return (
-		<section>
-			<div className="wrapper">
+		<section className={sectionStyles.booking}>
+			<div className={styles.bookingSuccess}>
 				<div className="left">
 					<h3>We are preparing for your visit!</h3>
-					<div>
+					<div className={styles.infoBlock}>
 						<p>Your booking date is</p>
 						<p>
 							{intlFormat(date, {
@@ -22,16 +23,9 @@ const BookingSuccess = () => {
 							}) ?? "could not be retrieved"}
 						</p>
 					</div>
-					<div>
+					<div className={styles.backBtn}>
 						<Link to={"/"}>Back to home &rarr;</Link>
 					</div>
-				</div>
-				<div className="right">
-					<img
-						src="/restaurant.jpg"
-						alt="Little Lemon Atmosphere"
-						loading="lazy"
-					/>
 				</div>
 			</div>
 		</section>
