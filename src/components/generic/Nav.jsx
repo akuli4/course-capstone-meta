cfaaa401e7fc8ecff0a61b529616da28d9daa117
Alan Ksenzov <fkdrsp@proton.me>
2023-01-13 16:35:29 +0100

    Rework: optimized design for mobile

--- src/components/generic/Nav.jsx
+++ src/components/generic/Nav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../../../lib/header.module.css";
-
+import Button from "../util/button/Button";
 const APP_LINKS = [
 	{
 		title: "Home",
@@ -29,15 +29,41 @@ const APP_LINKS = [
 	},
 ];
 const Nav = () => {
+	const [isOpen, setIsOpen] = React.useState(false);
+	function handleMenu() {
+		setIsOpen((prev) => !prev);
+	}
 	return (
 		<nav className={styles.navList}>
-			<ul>
+			<ul className={styles.desktopMenu}>
 				{APP_LINKS.map((link, index) => (
 					<li key={index} className={styles.navLink}>
 						<Link to={link.url}>{link.title}</Link>
 					</li>
 				))}
 			</ul>
+			<div className={styles.mobileNav}>
+				<button onClick={handleMenu}>
+					<img src="/icons/ham.svg" alt="Open menu" />
+				</button>
+				{isOpen && (
+					<div className={styles.mobileMenu}>
+						<ul>
+							<button
+								onClick={handleMenu}
+								style={{ fontSize: "14pt" }}
+							>
+								&#10060;
+							</button>
+							{APP_LINKS.map((link, index) => (
+								<li key={index} className={styles.navLink}>
+									<Link to={link.url}>{link.title}</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+				)}
+			</div>
 		</nav>
 	);
 };
